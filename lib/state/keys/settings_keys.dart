3bb121d8f9f6092e8b4c94afb2738aaa7dd02ab5
Michel Jelsma <m.jelsma1@gmail.com>
2026-03-09 20:09:15 +0100

    Hide statusbar logic

--- lib/state/keys/settings_keys.dart
+++ lib/state/keys/settings_keys.dart
@@ -6,12 +6,12 @@ import '../state_key.dart';
 
 abstract final class SettingsKeys {
   static final themeMode = StateKey.forEnum('settings.themeMode', ThemeMode.values, defaultValue: ThemeMode.system);
-  static final hideStatusBar = StateKey.boolean('settings.hideStatusBar');
+  static final showStatusBar = StateKey.boolean('settings.showStatusBar', defaultValue: true);
   static final showActivity = StateKey.boolean('settings.showActivity', defaultValue: true);
   static final persistLayout = StateKey.boolean('settings.persistLayout', defaultValue: true);
   static final language = StateKey.forKeyedEnum('settings.language', AppLanguage.values, defaultValue: AppLanguage.en);
 
   static final brokers = StateKey.fromJson<List<BrokerEntry>>('settings.brokers', defaultValue: const [], toJson: (list) => list.map((e) => e.toJson()).toList(), fromJson: (raw) => (raw as List).map((e) => BrokerEntry.fromJson(e as Map<String, dynamic>)).toList());
 
-  static final List<StateKey> all = [themeMode, hideStatusBar, showActivity, persistLayout, language, brokers];
+  static final List<StateKey> all = [themeMode, showStatusBar, showActivity, persistLayout, language, brokers];
 }
