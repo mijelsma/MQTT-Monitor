a34c9483298813334b52357d5426d942b90a64c2
Michel Jelsma <m.jelsma1@gmail.com>
2026-03-09 20:18:06 +0100

    Remember last visited settings page

--- lib/ui/settings/settings_screen.dart
+++ lib/ui/settings/settings_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../state/app_state.dart';
+import '../../state/keys/settings_keys.dart';
 import '../../theme/app_colors.dart';
 import 'panels/about_panel.dart';
 import 'panels/language_panel.dart';
@@ -9,17 +12,9 @@ import 'settings_item.dart';
 import 'layouts/settings_wide_layout.dart';
 import 'layouts/settings_narrow_layout.dart';
 
-class SettingsScreen extends StatefulWidget {
+class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
-  @override
-  State<SettingsScreen> createState() => _SettingsScreenState();
-}
-
-class _SettingsScreenState extends State<SettingsScreen> {
-  SettingsSection _current = SettingsSection.about;
-
-  // Settings items
   static final _items = <SettingsItem>[
     (section: SettingsSection.brokers, label: 'Brokers', icon: Icons.dns_rounded, gradient: AppColors.brokerGradient),
     (section: SettingsSection.ui, label: 'User Interface', icon: Icons.palette_outlined, gradient: AppColors.uiGradient),
@@ -27,21 +22,25 @@ class _SettingsScreenState extends State<SettingsScreen> {
     (section: SettingsSection.about, label: 'About', icon: Icons.info_outline_rounded, gradient: AppColors.aboutGradient),
   ];
 
-  Widget _buildPanel() => switch (_current) {
-    SettingsSection.brokers => const BrokersPanel(),
-    SettingsSection.ui => const UiPanel(),
-    SettingsSection.language => const LanguagePanel(),
-    SettingsSection.about => const AboutPanel(),
-  };
-
   @override
   Widget build(BuildContext context) {
+    final state = context.watch<AppStateManager>();
+    final current = state.read(SettingsKeys.activeSettingsSection);
     final isWide = MediaQuery.sizeOf(context).width >= 600;
 
+    void select(SettingsSection s) => context.read<AppStateManager>().write(SettingsKeys.activeSettingsSection, s);
+
+    Widget panel() => switch (current) {
+      SettingsSection.brokers => const BrokersPanel(),
+      SettingsSection.ui => const UiPanel(),
+      SettingsSection.language => const LanguagePanel(),
+      SettingsSection.about => const AboutPanel(),
+    };
+
     if (isWide) {
-      return SettingsWideLayout(items: _items, current: _current, onSelect: (s) => setState(() => _current = s), panel: _buildPanel());
+      return SettingsWideLayout(items: _items, current: current, onSelect: select, panel: panel());
     } else {
-      return SettingsNarrowLayout(items: _items, current: _current, onSelect: (s) => setState(() => _current = s), panel: _buildPanel());
+      return SettingsNarrowLayout(items: _items, current: current, onSelect: select, panel: panel());
     }
   }
 }
